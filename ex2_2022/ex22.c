84dd44f2416573ab13f5a366c029545c05e845a9
Yael Avioz <65490010+YaelAvioz@users.noreply.github.com>
2022-05-21 13:33:52 +0300

    Add files via upload

--- ex2_2022/ex22.c
+++ ex2_2022/ex22.c
@@ -26,7 +26,7 @@ void write_output(char *user_name, char *gradeAndReason, int results_file)
     write(results_file, "\n", strlen("\n"));
 }
 
-void compile(char *file_name, char *user_name, char *input, char *output, int results_file)
+void compile(char *filePath, char *user_name, char *input, char *output, int results_file)
 {
     int pid1 = fork();
 
@@ -35,22 +35,22 @@ void compile(char *file_name, char *user_name, char *input, char *output, int re
     {
         exit(-1);
     }
-    
-    //child case
+
+    // child case
     if (pid1 == 0)
     {
-        char *args[] = {"gcc", "-o", "compiled.out", file_name, NULL};
+        char *args[] = {"gcc", "-o", "compiled.out", filePath, NULL};
         execvp(args[0], args);
         exit(-1);
     }
 
-    //main process 
+    // main process
     else
     {
         int compile_res;
         pid_t waitpid1_res = waitpid(pid1, &compile_res, WCONTINUED);
 
-        //in case there was a failiure
+        // in case there was a failiure
         if (waitpid1_res < 0)
         {
             return;
@@ -65,13 +65,12 @@ void compile(char *file_name, char *user_name, char *input, char *output, int re
             {
                 exit(1);
             }
-            
+
             if (pid2 == 0)
             {
                 int input_fd = open(input, O_RDONLY);
                 int output_fd = open("output.txt", O_CREAT | O_WRONLY | O_TRUNC, 0644);
 
-                // in case there was a failiure
                 if ((input_fd < 0) || (output_fd < 0) || (dup2(input_fd, 0) < 0) || (dup2(output_fd, 1) < 0) || (execlp("./compiled.out", "./compiled.out", NULL) < 0))
                 {
                     exit(-1);
@@ -83,7 +82,7 @@ void compile(char *file_name, char *user_name, char *input, char *output, int re
                 sleep(5);
                 pid_t waitpid2_res = waitpid(pid2, &status, WNOHANG);
 
-                //in case there was a failiure
+                // in case there was a failiure
                 if (waitpid2_res < 0)
                 {
                     return;
@@ -104,7 +103,7 @@ void compile(char *file_name, char *user_name, char *input, char *output, int re
                         strcpy(correctOutput, output);
                         int pid = fork();
 
-                        //in case there was a failiure
+                        // in case there was a failiure
                         if (pid < 0)
                         {
                             exit(-1);
@@ -114,7 +113,7 @@ void compile(char *file_name, char *user_name, char *input, char *output, int re
                         {
                             char *args[] = {"./comp.out", "output.txt", correctOutput, NULL};
 
-                            //in case there was a failiure
+                            // in case there was a failiure
                             if (execvp(args[0], args) < 0)
                             {
                                 exit(-1);
@@ -129,24 +128,27 @@ void compile(char *file_name, char *user_name, char *input, char *output, int re
                                 return;
                             }
 
-                            //returns true if the child terminated normally
+                            // returns true if the child terminated normally
                             if (WIFEXITED(status))
                             {
-                                //returns the exit status of the child
+                                // returns the exit status of the child
                                 int child_status = WEXITSTATUS(status);
 
                                 // equal
-                                if(child_status == 1) {
+                                if (child_status == 1)
+                                {
                                     write_output(user_name, EXCELLENT, results_file);
                                 }
 
                                 // diffrant
-                                else if(child_status == 2){
+                                else if (child_status == 2)
+                                {
                                     write_output(user_name, WRONG, results_file);
                                 }
 
                                 // similar
-                                else if(child_status == 3){
+                                else if (child_status == 3)
+                                {
                                     write_output(user_name, SIMILAR, results_file);
                                 }
                             }
@@ -165,7 +167,6 @@ void compile(char *file_name, char *user_name, char *input, char *output, int re
 
 int main(int argc, char *argv[])
 {
-
     // validated input
     if (argc != 2)
     {
@@ -194,7 +195,7 @@ int main(int argc, char *argv[])
     }
 
     // create the errors file
-    int errors_file = open("errors.txt", O_CREAT | O_WRONLY, 0666);
+    int errors_file = open("errors.txt", O_WRONLY | O_CREAT, 0666);
 
     // in case there was a failiure
     if (errors_file < 0)
@@ -206,13 +207,14 @@ int main(int argc, char *argv[])
     close(errors_file);
 
     // initalized the data from buffer
-    char dir[MAXLEN];
+    char directory[MAXLEN];
     char input[MAXLEN];
     char output[MAXLEN];
+
     char *line;
 
     line = strtok(buffer, "\n");
-    strcpy(dir, line);
+    strcpy(directory, line);
 
     line = strtok(NULL, "\n");
     strcpy(input, line);
@@ -231,7 +233,9 @@ int main(int argc, char *argv[])
 
     // open directory
     DIR *dir_fd;
-    dir_fd = opendir(dir);
+    dir_fd = opendir(directory);
+
+    // in case there was a failiure
     if (dir_fd < 0)
     {
         return -1;
@@ -241,7 +245,6 @@ int main(int argc, char *argv[])
     struct dirent *entry;
     for (; entry = readdir(dir_fd); entry != NULL)
     {
-
         // skip non-directry entries
         if (entry->d_type != DT_DIR)
         {
@@ -261,26 +264,36 @@ int main(int argc, char *argv[])
         }
 
         // get subdir full path
-        char full_path[MAXLEN];
-        memset(full_path, 0, MAXLEN);
-        snprintf(full_path, MAXLEN, "%s/%s", dir, entry->d_name);
+        char dirPath[MAXLEN];
+        memset(dirPath, 0, MAXLEN);
+        snprintf(dirPath, MAXLEN, "%s/%s", directory, entry->d_name);
 
         DIR *subdir_fd;
-        subdir_fd = opendir(full_path);
+        subdir_fd = opendir(dirPath);
+
+        // in case there was a failiure
+        if (subdir_fd < 0)
+        {
+            return -1;
+        }
 
         int is_found = 0;
         struct dirent *subdir_entry;
+
         for (; subdir_entry = readdir(subdir_fd); subdir_entry != NULL)
         {
-            if (subdir_entry->d_type == DT_REG || subdir_entry->d_type == DT_UNKNOWN){
-
-                //checks if the file ends with .c
-                const char *dot = strrchr(subdir_entry->d_name, '.');
-                if ((!(!dot || dot == subdir_entry->d_name)) && (strcmp(dot + 1, "c") == 0)){
-                    char file_name[MAXLEN];
-                    memset(file_name, 0, MAXLEN);
-                    snprintf(file_name, MAXLEN, "%s/%s", full_path, entry->d_name);
-                    compile(file_name, entry->d_name, input, output, results_file);
+            if ((subdir_entry->d_type == DT_REG) || (subdir_entry->d_type == DT_UNKNOWN))
+            {
+                // checks if the file ends with .c
+                char *dot;
+                dot = strrchr(subdir_entry->d_name, '.');
+                if ((!(!dot || dot == subdir_entry->d_name)) && (strcmp((dot + 1), "c") == 0))
+                {
+                    // get subdir full path
+                    char filePath[MAXLEN];
+                    memset(filePath, 0, MAXLEN);
+                    snprintf(filePath, MAXLEN, "%s/%s", dirPath, subdir_entry->d_name);
+                    compile(filePath, entry->d_name, input, output, results_file);
                     is_found = 1;
                     break;
                 }
